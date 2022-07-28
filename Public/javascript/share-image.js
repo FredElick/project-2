7c58d01a6f157dbdf13ee9c732b5693423ab113c
Booker King <btking1@my.waketech.edu>
2022-07-28 11:36:45 -0400

    remove comments

--- Public/javascript/share-image.js
+++ Public/javascript/share-image.js
@@ -1,5 +1,4 @@
-const cloudinary =  ("../../config/cloud-connection");
-
+// SHOWS IMAGE PREVIEW
 const image_input = document.querySelector("#image-input");
 
 image_input.addEventListener("change", function () {
@@ -12,58 +11,19 @@ image_input.addEventListener("change", function () {
     ).style.backgroundImage = `url(${uploaded_image})`;
     return uploaded_image;
   });
+  //   THIS IS THE ACTUAL PATH
   reader.readAsDataURL(this.files[0]);
   return reader.result;
 });
 
+// ACTUAL SUBMIT FORM
 async function submitFormHandler(event) {
   event.preventDefault();
-  //   on  submit, get the image url
   const image_url = document.querySelector("#image-input").value;
+
+  //   THIS CONSOLES A FAKE  PATH..??
   console.log(image_url);
-
-  // use reader to get the image url
-//   const reader = new FileReader();
-
-//   reader
-//     .addEventListener("load", () => {
-//       const uploaded_image = reader.result;
-//       console.log(uploaded_image);
-//       document.querySelector(
-//         "#display-image"
-//       ).style.backgroundImage = `url(${uploaded_image})`;
-//       return uploaded_image;
-//     })
-//     .readAsDataURL(image_url);
-//   use cloudinary to upload the image
-    const uploaded_image = await cloudinary.uploader.upload(image_url);
-  console.log(uploaded_image);
 }
-
-//   reader.readAsDataURL(image_input.files[0]);
-
-// //   get result of reader.readAsDataURL
-//     const uploaded_image = await reader.result;
-//     console.log(uploaded_image);
-// //   const formData = new FormData();
-//   formData.append("image", image.files[0]);
-//   console.log(reader.result)
-//   const image = reader.readAsDataURL(image_input.files[0]);
-
-//   console.log(image.files[0]);
-//   const response = await fetch("api/posts/image", {
-//     method: "POST",
-//     body: image.files[0],
-//     headers: {
-//       "Content-Type": "multipart/form-data",
-//     },
-//   });
-//   if (response.ok) {
-//     const result = await response.json();
-//     console.log(result);
-//   } else {
-//     console.log(response.statusText);
-//   }
 
 document
   .querySelector(".share-image-form")
