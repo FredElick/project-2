d74e12d4debf347b651054b04177b721f220a3ad
Booker King <btking1@my.waketech.edu>
2022-07-31 13:37:34 -0400

    add .env variables

--- config/cloud-connection.js
+++ config/cloud-connection.js
@@ -4,7 +4,7 @@ const cloudinary = require('cloudinary').v2;
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECERT,
+  api_secret: process.env.API_SECRET,
 });
 
 // console.log(cloudinary.config())
