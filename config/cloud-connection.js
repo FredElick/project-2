378daa8cc5d64b80c690a20b11994d7a0b705ccd
Booker King <btking1@my.waketech.edu>
2022-07-30 22:36:19 -0400

    add env variables

--- config/cloud-connection.js
+++ config/cloud-connection.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 
 // Return "https" URLs by setting secure: true
 cloudinary.config({
-  cloud_name: "btking1",
-  api_key: "386916492395731",
-  api_secret: "cip3D05tfXRGaTUUw9aIFqJX7w0",
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECERT,
 });
 
 // console.log(cloudinary.config())
